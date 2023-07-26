2f3e3632f5b04a2df89361b04ebdc91cc3f42259
Stephgarro <109922003+Stephgarro@users.noreply.github.com>
2023-07-26 10:55:19 -0600

    feat(js):add new functions

---
+++ js/projects.js
@@ -0,0 +1,65 @@
+const REDES_SOCIALES = {
+  github: 'https://github.com/Stephgarro',
+  linkedin: 'https://www.linkedin.com/in/stephannie-garro-miranda-0a0936259/',
+  correo: 'contact@stepgarro2001.com',
+}
+
+
+
+const TRABAJOS = [
+  {
+    nombre: 'Learning',
+    fecha: '06 - december - 2022',
+    descripcion:
+      'This is about learning web page, Here you can expand your knowledge and develop new skills with the wide variety of online courses using only html and css.',
+    website: 'https://proyecto-web-final.vercel.app/',
+    github: 'https://github.com/ValeriaMuSe/proyecto-final-web',
+    imagen: './images/learning-project.png'
+  },
+  {
+    nombre: 'Go traveller',
+    fecha: '3 - may - 2023',
+    descripcion:
+      ' Landing page about travels using html, css and javascript.',
+    website: 'https://hotel-project-eight.vercel.app/',
+    github: 'https://github.com/ValeriaMuSe/global-traveller-project',
+    imagen: './images/Global-project.png'
+  },
+  {
+    nombre: 'pocke app',
+    fecha: '07 - april - 2023',
+    descripcion:
+      "Application web called pockeapp using html, css and javascript",
+    website: 'https://pockeapp.vercel.app/?vercelToolbarCode=XP9z-sjM_k2kjvG',
+    github: 'https://github.com/Stephgarro/javascript-2-grupo-1--stephannieGarro-/tree/main/pokeApp',
+    imagen: './images/pokeapp-project.png'
+  },
+  {
+    nombre: 'Portfolio',
+    fecha: '10 - agust - 2022',
+    descripcion:
+      'first version of portfolio olny with html and css.',
+    website: 'proyecto-final-web-sandy.vercel.app',
+    github: 'https://proyecto-final-web-sandy.vercel.app/',
+    imagen: './images/portfolio.png'
+  },
+  {
+    nombre: 'skin care ',
+    fecha: '17 - november - 2022',
+    descripcion:
+      'Landing page about skin care , only with html, css and sass.',
+    website: 'https://github.com/DefArs/laboratorio2',
+    github: 'https://github.com/DefArs/laboratorio2',
+    imagen: './images/skincare-project.png'
+  },
+  {
+    nombre: 'Hotels',
+    fecha: '06 - march- 2023',
+    descripcion:
+      'Landong page about hotels with html,css,and javascript.',
+    website: 'https://juc24.github.io/proyecto1_proyecto-final/',
+    github: 'https://github.com/JUC24/proyecto1_proyecto-final',
+    imagen: './images/home-page.png'
+  },
+
+]
